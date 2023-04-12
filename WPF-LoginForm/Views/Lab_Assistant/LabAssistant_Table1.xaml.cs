2ec63dd21d6597176596344d2b79f39687aff3e9
Jakub Hoś <72313322+jakub22222@users.noreply.github.com>
2023-04-12 20:36:51 +0200

    View

---
+++ WPF-LoginForm/Views/Lab_Assistant/LabAssistant_Table1.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPF_LoginForm.Views.Lab_Assistant
+{
+    /// <summary>
+    /// Logika interakcji dla klasy LabAssistant_Table1.xaml
+    /// </summary>
+    public partial class LabAssistant_Table1 : Window
+    {
+        public LabAssistant_Table1()
+        {
+            InitializeComponent();
+        }
+
+        private void ListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            InitializeComponent();
+            List<User> items = new List<User>();
+            items.Add(new User() { Name = "John Doe", Age = 42, Mail = "john@doe-family.com" });
+            items.Add(new User() { Name = "Jane Doe", Age = 39, Mail = "jane@doe-family.com" });
+            items.Add(new User() { Name = "Sammy Doe", Age = 7, Mail = "sammy.doe@gmail.com" });
+            //lvUsers.ItemsSource = items;
+        }
+    }
+
+    public class User
+    {
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public string Mail { get; set; }
+    }
+}
